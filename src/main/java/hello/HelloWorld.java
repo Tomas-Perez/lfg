47b2d313839e673bd822a6c4d33224b9602585a0
Manuel Pedrozo <manuel.pedrozo@ing.austral.edu.ar>
2018-03-22 12:24:30 -0300

    added hibernate

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,8 @@
 package hello;
+import org.hibernate.Session;
 
 public class HelloWorld {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     System.out.println("Hello World!");
   }
-}
+}   
