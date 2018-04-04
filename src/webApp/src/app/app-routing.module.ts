df49c822fed451ab6f8e12fbff6622dde0aa1e3c
Manuel Pedrozo <manuel.pedrozo@ing.austral.edu.ar>
2018-04-04 09:29:51 -0300

    Added routes

--- src/webApp/src/app/app-routing.module.ts
+++ src/webApp/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SignInComponent} from './sign-in/sign-in.component';
 import {HomeNavbarComponent} from './home-navbar/home-navbar.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full'},
+  { path: 'sign-in', component: SignInComponent},
+  { path: 'sign-up', component: SignUpComponent},
   { path: 'home', component: HomeNavbarComponent}
 ];
 
