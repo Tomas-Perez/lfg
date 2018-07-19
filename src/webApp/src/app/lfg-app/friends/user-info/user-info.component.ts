c7e9cdbaff6c878103590d0e75b114f6d2ebddab
lethanity <manuel.pedrozo@ing.austral.edu.ar>
2018-07-19 00:53:51 -0300

    Fix user-info import onDestroy

--- src/webApp/src/app/lfg-app/friends/user-info/user-info.component.ts
+++ src/webApp/src/app/lfg-app/friends/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FriendService} from '../../../_services/friend.service';
 import {BasicUser} from '../../../_models/BasicUser';
